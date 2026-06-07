3bf3c90f48bf5059395a085b93000d265b3a6aea
david7526ha <dave.yedam.ha@gmail.com>
2026-06-07 16:18:01 -0400

    fix: disable SSR to prevent hydration errors

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
-import Main from "../components/Main";
+import dynamic from "next/dynamic";
+
+const Main = dynamic(() => import("../components/Main"), { ssr: false });
+
 export default function Page() {
   return <Main />;
 }
