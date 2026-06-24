bcb1adf198e7af4cf0cee0fc19014312769b89ef
david7526ha <dave.yedam.ha@gmail.com>
2026-06-24 01:33:43 -0400

    temp: include users groupPicks for analysis

--- app/api/debug-state/route.js
+++ app/api/debug-state/route.js
@@ -1,8 +1,16 @@
 import { adminDb } from '../../../lib/firebaseAdmin';
 
 export async function GET() {
-  const data = (await adminDb.collection('tournament').doc('state').get()).data() || {};
-  return new Response(JSON.stringify({matchResults:data.matchResults||{}, groupResults:data.groupResults||{}}), {
-    headers: {'Content-Type':'application/json'}
+  const tDoc = (await adminDb.collection('tournament').doc('state').get()).data() || {};
+  const usersSnap = await adminDb.collection('users').get();
+  const users = {};
+  usersSnap.forEach(doc => { 
+    const d = doc.data();
+    users[doc.id] = { name: d.name, groupPicks: d.groupPicks||{} };
   });
+  return new Response(JSON.stringify({
+    matchResults: tDoc.matchResults||{}, 
+    groupResults: tDoc.groupResults||{},
+    users
+  }), { headers: {'Content-Type':'application/json'} });
 }
